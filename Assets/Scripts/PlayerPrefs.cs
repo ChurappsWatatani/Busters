bf64adaac33a0c374f09cfdeebad7a67e8adcd07
Yuri <watatani.yuri@churapps.co.jp>
2017-09-01 21:01:28 +0900

    Scriptsフォルダーコミット

---
+++ Assets/Scripts/PlayerPrefs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefs {
+	private static PlayerPrefs instance_;
+
+	public static PlayerPrefs Instance(){
+		if(instance_ == null){
+			instance_ = new PlayerPrefs();
+		}
+		return instance_;
+	}
+}
