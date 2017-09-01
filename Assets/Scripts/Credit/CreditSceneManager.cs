f1706433c79d962d70da0c387bdbe271da40a860
Shuhei YAMADA <yamada.shuhei@churapps.co.jp>
2017-09-01 23:35:56 +0900

    クレジット画面とひみつ画面

---
+++ Assets/Scripts/Credit/CreditSceneManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditSceneManager : MonoBehaviour {
+
+	public void OnTouchTitleButton()
+	{
+		SceneManager.LoadScene("Title");
+	}
+}
---
+++ Assets/Scripts/Title/TitleSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour {
+
+	public void OnPushStartButton()
+	{
+		SceneManager.LoadScene("Game");
+	}
+
+	public void OnPushCreditButton()
+	{
+		SceneManager.LoadScene("Credit");
+	}
+	public void OnPushSecretButton()
+	{
+		SceneManager.LoadScene("Secret");
+	}
+}
