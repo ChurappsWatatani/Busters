a31f04faca4ab24458150c2d9d5a65410680a58c
Shuhei YAMADA <yamada.shuhei@churapps.co.jp>
2017-09-02 11:20:40 +0900

    クレジット画面実装した

--- Assets/Scripts/03_Credit/CreditSceneManager.cs
+++ Assets/Scripts/03_Credit/CreditSceneManager.cs
@@ -2,11 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using DG.Tweening;
 public class CreditSceneManager : MonoBehaviour {
 
+	[SerializeField] private float _distance;
+	[SerializeField] private float _moveDuration;
+	[SerializeField] private float _buttonDuration;
+	[SerializeField] private GameObject _panel;
+	[SerializeField] private Image _buttonImage;
 	public void OnTouchTitleButton()
 	{
 		SceneManager.LoadScene("01_Title");
+	}
+
+	public void Start()
+	{
+		DOTween.ToAlpha(
+			() => _buttonImage.color,
+			color => _buttonImage.color = color,
+			0,
+			0
+		);
+
+		_panel.transform.DOLocalMoveY(_distance, _moveDuration)
+			.SetRelative()
+			.SetEase(Ease.Linear)
+			.OnComplete(() => DOTween.ToAlpha(
+				() => _buttonImage.color,
+				color => _buttonImage.color = color,
+				1f,
+				_buttonDuration
+			));
 	}
 }
