083e4c9b397056b6167bdd6b92e0a0020c0de89d
Yuri <watatani.yuri@churapps.co.jp>
2017-09-02 12:40:18 +0900

    ステージ読み込み機能追加

--- Assets/Scripts/04_Game/GameManager.cs
+++ Assets/Scripts/04_Game/GameManager.cs
@@ -16,20 +16,28 @@ public class GameManager : CASingletonMonoBehaviour<GameManager> {
 		EndAnimation,
 		Result
 	}
-	public int stageCount = 1;
 	
 	public int point = 0;
 	public int playCount = 0;
-	public PlayerController player;
-	public List<GameObject> garbages = new List<GameObject>();
 	public Dictionary<string, int> busterGarbages = new Dictionary<string, int>();
 	public State gameState = State.Start;
+
+	protected int stageCount = 1;
+	protected StageManager stageManager;
+	protected PlayerController player;
+	protected List<GameObject> garbages = new List<GameObject>();
 	protected StageAnimationController animator;
 
 	public void Start()
 	{
-		Debug.Log("Start");
-		Initialize();
+		gameState = State.Start;
+	}
+
+	public void Update()
+	{
+		if (gameState == State.Start) {
+			Initialize();
+		}
 	}
 
 	public void Initialize()
@@ -39,11 +47,28 @@ public class GameManager : CASingletonMonoBehaviour<GameManager> {
 		gameState = State.Start;
 
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-		garbages = GameObject.FindGameObjectsWithTag("Garbage").ToList();
 		animator = GameObject.FindGameObjectWithTag("StageAnimation").GetComponent<StageAnimationController>();
+		stageManager = GameObject.FindGameObjectWithTag("StageManager").GetComponent<StageManager>();
 
-		//本来はステージごとにwarningに行くかスタートするか決める
-		GameStart();
+		stageCount = PlayerPrefs.GetInt(PlayerPrefsKey.SELECTED_STAGE_KEY, 1);
+		var stageParent = GameObject.FindGameObjectWithTag("Stage");
+		var obj = Instantiate(Resources.Load("Prefabs/Stage" + stageCount)) as GameObject;
+		obj.transform.SetParent(stageParent.transform);
+		obj.transform.localScale = Vector3.one;
+		obj.transform.localPosition = new Vector2(0, -57);
+		var stage = obj.GetComponent<StageController>();
+		stageManager.stageText.text = "ステージ" + stageCount;
+		stageManager.stageNameText.text = stage.stageName;
+
+		//ステージ内のゴミリスト取得
+		garbages = GameObject.FindGameObjectsWithTag("Garbage").ToList();
+
+		if (stageCount < 8) {
+			GameStart();
+		}
+		else {
+			animator.EnterWarning();
+		}
 	}
 
 	public void GameStart()
@@ -71,6 +96,7 @@ public class GameManager : CASingletonMonoBehaviour<GameManager> {
 		}
 		garbages.Remove(obj);
 		if (garbages.Count <= 0) {
+			Debug.Log("ゴミ終了");
 			GameEnd();
 		}
 	}
@@ -78,7 +104,8 @@ public class GameManager : CASingletonMonoBehaviour<GameManager> {
 	public bool CheckEndPlayCount()
 	{
 		if (MAX_PLAY_COUNT <= playCount) {
-			GameEnd();
+            GameEnd();
+			Debug.Log("残機なし");
 		}
 		return (MAX_PLAY_COUNT <= playCount);
 	}
