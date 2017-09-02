ff19e960c16746a10cc9a1b1760747faa1dd8e25
Shuhei YAMADA <yamada.shuhei@churapps.co.jp>
2017-09-02 11:35:29 +0900

    タイトル画面でクレジットボタンとひみつボタンを隠す処理を追加した

--- Assets/Scripts/01_Title/TitleSceneManager.cs
+++ Assets/Scripts/01_Title/TitleSceneManager.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleSceneManager : MonoBehaviour {
+	[SerializeField] private bool _showCreditButton;
+	[SerializeField] private bool _showSecretButton;
+	[SerializeField] private GameObject _creditButton;
+	[SerializeField] private GameObject _secretButton;
+	void Start()
+	{
+		if(PlayerPrefs.GetInt(PlayerPrefsKey.CREDIT_KEY) == 1 || _showCreditButton)
+		{
+			_creditButton.SetActive(true);
+		}
+		if(PlayerPrefs.GetInt(PlayerPrefsKey.SECRET_KEY) == 1 || _showSecretButton)
+		{
+			_secretButton.SetActive(true);
+		}
 
+	}
 	public void OnPushStartButton()
 	{
 		SceneManager.LoadScene("04_Game");
---
+++ Assets/Scripts/PlayerPrefsKey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsKey {
+
+	//クレジット画面をオープンするかどうかのフラグを格納する
+	public static string CREDIT_KEY = "Credit";
+	//ひみつ画面をオープンするかどうかのフラグを格納する
+	public static string SECRET_KEY = "Secret";
+	// private static PlayerPrefs instance_;
+
+	// public static PlayerPrefs Instance(){
+	// 	if(instance_ == null){
+	// 		instance_ = new PlayerPrefs();
+	// 	}
+	// 	return instance_;
+	// }
+}
