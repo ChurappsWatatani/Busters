c09d9a96797e8769ad19d28a801f67f50c985ca8
Appbeach <system@app-beach.com>
2017-09-02 17:06:51 +0900

    ステージ５追加

---
+++ Assets/Scripts/04_Game/stage8/CatMove2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMove2 : MonoBehaviour 
+{
+	private float posY;
+
+	private float speedX = -3;
+	private float speedY = 3;
+
+
+	void Update()
+	{
+		this.transform.localPosition += new Vector3 (speedX,speedY, 0);
+
+		if (this.transform.localPosition.x < -250 || this.transform.localPosition.x > 250) {
+			speedX *= -1;
+		}
+
+		if (this.transform.localPosition.y < -400 || this.transform.localPosition.y > 400) {
+			speedY *= -1;
+		}
+	}
+}
