38c6b401c49178da4b9ff6c9e5b20912a3539ccd
Appbeach <system@app-beach.com>
2017-09-02 16:26:44 +0900

    ステージ８追加

---
+++ Assets/Scripts/04_Game/stage8/CatMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMove : MonoBehaviour 
+{
+	
+	private float posY;
+	void Awake()
+	{
+		posY = this.transform.localPosition.y;
+
+	}
+
+	void Update()
+	{
+		this.transform.localPosition += new Vector3 (-10, 0, 0);
+
+		if (this.transform.localPosition.x < -400) {
+			this.transform.localPosition = new Vector3 (400, posY, 0);
+		}
+	}
+}
